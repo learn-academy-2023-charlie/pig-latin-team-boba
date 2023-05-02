91d4d49f99512b02dc12b0008cfc0118e3f50820
bkre12345 <belvira@terpmail.umd.edu>
2023-05-02 17:30:00 -0400

    pig latin translator is translating

--- src/App.js
+++ src/App.js
@@ -3,28 +3,50 @@ import './App.css';
 import butcherPigImage from './assets/butcherPig.jpeg';
 
 const App = () => {
-
-  const [userInput, setUserInput] = useState("apple through queen squeal fry fluent");
-  const [inputTranslated, setInputTranslated] = useState("");
+  const [userInput, setUserInput] = useState("apple through queen squeal fry fluent")
+  const [inputTranslated, setInputTranslated] = useState("")
 
   const myPigLatinCodeHere = () => {
-    const arrayOfUserInput = userInput.split(" ");
-    const translatedWordsArray = arrayOfUserInput.map(word => {
-      const eachWord = word[0];
-      if (/[aeiou]/i.test(eachWord)) {
-        return (word + "way");
+    const arrayOfUserInput = userInput.split(" ")
+    const translatedWordsArray = arrayOfUserInput.map((eachWord) => {
+      const vowelsArray = eachWord.split("").filter((vowel) => {
+        return (
+          vowel === "a" || 
+          vowel === "e" || 
+          vowel === "i" || 
+          vowel === "o" || 
+          vowel === "u")
+      })
+      const firstVowelIndex = eachWord.indexOf(vowelsArray[0]);
+      if (firstVowelIndex === 0) {
+        return eachWord + "way";
       } else {
-        return (word.substring(1) + eachWord + "ay");
+        const beginningConsonants = eachWord.slice(0, firstVowelIndex);
+        const restOfWord = eachWord.slice(firstVowelIndex);
+        return restOfWord + beginningConsonants + "ay";
       }
     });
-    setInputTranslated(translatedWordsArray.join(" "));
+    const translatedWords = translatedWordsArray.join(" ");
+    console.log("translatedWords:", translatedWords)
+    setInputTranslated(translatedWords);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    myPigLatinCodeHere();
-  };
-  
+  // // ACTION ITEM: this method restarts the game by setting the original state, when you are ready for your full user experience delete the test words in setUserInput and pass an empty string
+  // const restartGame = () => {
+  //   setUserInput("apple through queen squeal fry fluent")
+  //   setInputTranslated("")
+  // }
+
+  // NO MODIFICATION NEEDED: this method prevents React from refreshing the page unnecessarily
+  const setUpPreventDefault = (e) => {
+    e.preventDefault()
+    myPigLatinCodeHere()
+  }
+
+  // NO MODIFICATION NEEDED: this method takes the value of the input and saves it in state
+  const handleInput = (e) => {
+    setUserInput(e.target.value)
+  } 
   return (
     <div className="page-container">
       <div className="body-container">
@@ -37,135 +59,21 @@ const App = () => {
 
         <div className="input-section">
           <h4>Enter phrase to be translated:</h4>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              className="user-input"
-              value={userInput}
-              onChange={(e) => setUserInput(e.target.value)}
-            />
-            <br />
-            <button type="submit">Submit</button>
-          </form>
+          <input
+            type="text"
+            className="user-input"
+            onChange={handleInput}
+            value={userInput}
+          />
+          <br />
+          <button onClick={setUpPreventDefault}>Submit</button>
+          {/* <button onClick={restartGame}>Clear</button> */}
         </div>
         <p>{inputTranslated}</p>
       </div>
-      <footer>&copy; 2022 | Coded by: Yue Han & Bea</footer>
+      <footer>&copy; 2022 | Coded by: Your Names Here!</footer>
     </div>
-  );
-};
-
-
-
-
-
-  // const myPigLatinCodeHere = () => {
-
-  //   const arrayOfUserInput = userInput.split(" ")
-  //   console.log("arrayOfUserInput:", arrayOfUserInput)
-
-  //   const translatedWordsArray = arrayOfUserInput.map(eachWord => {
-  //     console.log("eachWord:", eachWord) 
-  //     const vowelsArray = eachWord.split("").filter(vowel => {
-  //       return (
-  //         vowel === "a" || 
-  //         vowel === "e" || 
-  //         vowel === "i" || 
-  //         vowel === "o" || 
-  //         vowel === "u"
-  //        })
-  //     })
-  //         console.log("vowelsArray:", vowelsArray)
-
-  //           const firstVowel = vowelsArray.substring(0)
-  //           const restofWords = eachWord.slice(eachWord.indexOf(firstVowel)) 
-  //              if (/[aeiou]/i.test(firstVowel)) 
-  //               return ((restofWords + firstVowel + "way").join(""))
-  //               else {
-  //                 return eachWord
-  //              }
-  //             }
-  //           const translatedWords = setInputTranslated(translatedWordsArray.join(" "))
-        
-          // "string" "tring"
-          // String.substring(1)
-
-      // ACTION ITEM: your Pig Latin logic goes here!
-
-      // vowel-function
-      // - if the words beginning with a vowel, 
-      //  return add "way" to the end.
-
-  
-
-      // consonant functionality
-      // - if beginning with one or more consonants, 
-      // return by moving all of the first consecutive consonants to the end 
-      // return add "ay".
-
-      // qu functionality
-      // - If the first consonants include "qu", 
-      // then move the "u" along with the "q". (Don't forget about words like "squeal" where "qu" doesn't come first!)
-
-      // y functionality
-
-    
-
-      // ACTION ITEM: this return will be the output of your Pig Latin'd code
-  //     return eachWord
-  //   })
-
-  //   // NO MODIFICATION NEEDED: once the code has been modified it gets joined from an array back to a string
-  //   const translatedWords = translatedWordsArray.join(" ")
-  //   console.log("translatedWords:", translatedWords)
-
-  //   // NO MODIFICATION NEEDED: this will update the inputTranslated variable in state
-  //   setInputTranslated(translatedWords)
-  // }
-
-  // // ACTION ITEM: this method restarts the game by setting the original state, when you are ready for your full user experience delete the test words in setUserInput and pass an empty string
-  // const restartGame = () => {
-  //   setUserInput("apple through queen squeal fry fluent")
-  //   setInputTranslated("")
-  // }
-
-  // // NO MODIFICATION NEEDED: this method prevents React from refreshing the page unnecessarily
-  // const setUpPreventDefault = (e) => {
-  //   e.preventDefault()
-  //   myPigLatinCodeHere()
-  // }
-
-  // NO MODIFICATION NEEDED: this method takes the value of the input and saves it in state
-//   const handleInput = (e) => {
-//     setUserInput(e.target.value)
-//   } 
-//   return (
-//     <div className="page-container">
-//       <div className="body-container">
-//         <h1>Pig Latin Translator</h1>
-//         <img
-//           src={butcherPigImage}
-//           alt="pig with butcher cut names in pig latin"
-//           className="butcher-pig-image"
-//         />
-
-//         <div className="input-section">
-//           <h4>Enter phrase to be translated:</h4>
-//           <input
-//             type="text"
-//             className="user-input"
-//             onChange={handleInput}
-//             value={userInput}
-//           />
-//           <br />
-//           <button onClick={setUpPreventDefault}>Submit</button>
-//           <button onClick={restartGame}>Clear</button>
-//         </div>
-//         <p>{inputTranslated}</p>
-//       </div>
-//       <footer>&copy; 2022 | Coded by: Your Names Here!</footer>
-//     </div>
-//   )
-// }
+  )
+}
 
 export default App
